5fef243bfd540c288e2cf8c1646233c90fbf0e8b
JeanVittory <jean.carjil@gmail.com>
2022-06-26 20:25:52 -0500

    connecting with heroku

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { v4 } from "uuid";
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import styles from "../styles/Home.module.css";
 import { message } from "../types/message";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 let socket: any;
 const Home: NextPage = () => {
@@ -11,7 +15,7 @@ const Home: NextPage = () => {
     message: "",
   });
   const [dataFromSocket, setDataFromSocket] = useState<message[]>([]);
-  //const [socketConnection, setSocketConnection] = useState(false);
+
 
   useEffect(() => {
     socketInitializer();
@@ -48,19 +52,40 @@ const Home: NextPage = () => {
   };
   
   const handleSendMessage = () => {
-    //setSocketConnection(true);
+    if(!messageToSocket.username){
+      toast('username field must be filled', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return
+    }
     if (messageToSocket) {
       socket.emit("sendMessage", messageToSocket);
     }
   };
 
-  console.log(dataFromSocket)
   return (
     <div className={styles.container}>
-      <section>
+       <ToastContainer />
+      <Head>
+        <title>Chat App</title>
+      </Head>
+      <section className={styles.chat}>
         { 
-          dataFromSocket.map((e, idx)=>{
-            return <p key={idx}>{e.username}</p>
+          dataFromSocket.map( (e, idx) =>{
+            return (
+              <div key={v4()} className={idx % 2 !== 0 ?  styles.messageContainerRight:styles.messageContainerLeft}>
+                <div key={v4()}>
+                  <p key={v4()} className={styles.username}>{e.username || "unknown"}:</p>
+                  <p key={v4()} className={styles.message}>{e.message}</p>
+                </div>
+              </div>
+            )
           }
         )}
       </section>
